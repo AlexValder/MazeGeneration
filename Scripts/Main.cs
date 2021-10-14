bcadad9c3ef288703d08303b5c512ff88bc5334c
AlexValder <anna.loban@yahoo.com>
2021-10-14 21:37:46 +0300

    Settings

--- Scripts/Main.cs
+++ Scripts/Main.cs
@@ -1,96 +1,99 @@
-using Godot;
 using System;
 using System.Collections.Generic;
-using System.Linq;
+using Godot;
 using Serilog;
 
-public class Main : Spatial {
-    private List<Spatial> _rooms = new List<Spatial>();
-    private PackedScene _roomScene;
-    private readonly Random _random = new Random();
+namespace Demonomania.Scripts {
+    public class Main : Spatial {
+        private List<Spatial> _rooms = new List<Spatial>();
+        private PackedScene _roomScene;
+        private readonly Random _random = new Random();
 
-    public override void _Ready() {
-        SetupLogger();
+        public override void _Ready() {
+            SetupLogger();
 
-        _roomScene = GD.Load<PackedScene>("Scenes/Room.tscn");
+            _roomScene = GD.Load<PackedScene>("Scenes/Room.tscn");
 
-        OS.WindowMaximized = true;
-    }
-
-    public override void _UnhandledInput(InputEvent @event) {
-        if (@event.IsActionPressed("debug_exit")) {
-#if DEBUG
-            GetTree().Quit(0);
-#endif
+            OS.WindowMaximized = Settings.WindowMaximized;
         }
-    }
 
-    public void CreateRooms(int count) {
-        ClearRooms();
-        lock (_rooms) {
-            var camera = GetViewport().GetCamera();
-            // assuming that each "room" is 1x1 cube + 0.25 as a space between rooms
-            for (var i = 0; i < count; ++i) {
-                for (var j = 0; j < count; ++j) {
-                    var node = _roomScene.Instance<Spatial>();
-                    AddChild(node);
-                    node.GlobalTranslate(
-                        new Vector3(
-                            x: 2 * i,
-                            y: .5f,
-                            z: 2 * j
-                        )
-                    );
-                    var mesh = node.GetChild<MeshInstance>(0);
-                    mesh.MaterialOverride = new SpatialMaterial {
-                        AlbedoColor = new Color(
-                            r: _random.Next(0, 255) / 255f,
-                            g: _random.Next(0, 255) / 255f,
-                            b: _random.Next(0, 255) / 255f,
-                            a: 1f
-                        )
-                    };
-                    var label = node.GetChild<Label>(1);
-                    label.Text = (i * count + j + 1).ToString();
-                    label.SetPosition(
-                        camera.UnprojectPosition(node.GlobalTransform.origin + Vector3.Up / 2)
-                        - label.RectSize / 2
-                    );
-                    _rooms.Add(node);
+        public override void _UnhandledInput(InputEvent @event) {
+            if (@event.IsActionPressed("debug_exit")) {
+#if DEBUG
+                GetTree().Quit(0);
+#endif
+            }
+        }
+
+        public void CreateRooms(int count) {
+            ClearRooms();
+            lock (_rooms) {
+                var camera = GetViewport().GetCamera();
+                // assuming that each "room" is 1x1 cube + 0.25 as a space between rooms
+                for (var i = 0; i < count; ++i) {
+                    for (var j = 0; j < count; ++j) {
+                        var node = _roomScene.Instance<Spatial>();
+                        AddChild(node);
+                        node.GlobalTranslate(
+                            new Vector3(
+                                x: 2 * j,
+                                y: .5f,
+                                z: 2 * i
+                            )
+                        );
+                        var mesh = node.GetChild<MeshInstance>(0);
+                        if (mesh.Mesh.SurfaceGetMaterial(0).Duplicate() is SpatialMaterial material) {
+                            material.AlbedoColor = new Color(
+                                r: _random.Next(0, 255) / 255f,
+                                g: _random.Next(0, 255) / 255f,
+                                b: _random.Next(0, 255) / 255f,
+                                a: 1f
+                            );
+                            mesh.MaterialOverride = material;
+                        }
+
+                        var label = node.GetChild<Label>(1);
+                        label.Text = (i * count + j + 1).ToString();
+                        label.SetPosition(
+                            camera.UnprojectPosition(node.GlobalTransform.origin + Vector3.Up / 2)
+                            - label.RectSize / 2
+                        );
+                        _rooms.Add(node);
+                    }
+                }
+
+                if (_rooms.Count > 0) {
+                    _rooms[0].Scale = new Vector3(1, 2, 1);
+                    _rooms[0].Translate(new Vector3(0, 0.5f, 0));
                 }
             }
+        }
 
-            if (_rooms.Count > 0) {
-                _rooms[0].Scale = new Vector3(1, 2, 1);
-                _rooms[0].Translate(new Vector3(0, 0.5f, 0));
+        public void ClearRooms() {
+            lock (_rooms) {
+                if (_rooms.Count < 1) {
+                    return;
+                }
+
+                foreach (var room in _rooms) {
+                    RemoveChild(room);
+                    room.Free();
+                }
+                _rooms.Clear();
             }
         }
-    }
 
-    public void ClearRooms() {
-        lock (_rooms) {
-            if (_rooms.Count < 1) {
-                return;
-            }
-
-            foreach (var room in _rooms) {
-                RemoveChild(room);
-                room.Free();
-            }
-            _rooms.Clear();
-        }
-    }
-
-    private static void SetupLogger() {
-        var config = new LoggerConfiguration()
-                .WriteTo.Console()
+        private static void SetupLogger() {
+            var config = new LoggerConfiguration()
+                    .WriteTo.Console()
 #if DEBUG
-                .MinimumLevel.Debug()
+                    .MinimumLevel.Debug()
 #else
                 .MinimumLevel.Warning()
 #endif
-            ;
+                ;
 
-        Log.Logger = config.CreateLogger();
+            Log.Logger = config.CreateLogger();
+        }
     }
 }
---
+++ Scripts/Settings.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using Godot;
+using Serilog;
+using File = System.IO.File;
+
+namespace Demonomania.Scripts {
+    internal static class Settings {
+        private const string CONFIG_FILE = "config.ini";
+        private static readonly ConfigFile s_config = new ConfigFile();
+
+#region Setting Properties
+    #region Graphics
+        public static bool WindowMaximized {
+            get => GetValue<bool>(Section.Graphics, "window_maximized");
+            set => SetValue(Section.Graphics, "window_maximized", value);
+        }
+
+    #endregion
+#endregion
+
+        static Settings() {
+            if (!File.Exists(CONFIG_FILE)) {
+                using (_ = File.CreateText(CONFIG_FILE)) {}
+
+                CreateDefault();
+                return;
+            }
+
+            var result = s_config.Load(CONFIG_FILE);
+            if (result != Error.Ok) {
+                Log.Logger.Error("Failed to load config file. Error: {Error}", result);
+            }
+        }
+
+        private static void CreateDefault() {
+            foreach (var section in Defaults) {
+                foreach (var key in section.Value) {
+                    s_config.SetValue(section.Key.AsString(), key.Key, key.Value);
+                }
+            }
+            Save();
+        }
+
+        private static void Save() {
+            var result = s_config.Save(CONFIG_FILE);
+            if (result != Error.Ok) {
+                Log.Logger.Error("Failed to save config file");
+            }
+        }
+
+        private static T GetValue<T>(Section section, string field) {
+            var value = s_config.GetValue(section.AsString(), field);
+            return value is T typedValue ? typedValue : default;
+        }
+        private static void SetValue<T>(Section section, string field, T value) {
+            s_config.SetValue(section.AsString(), field, value);
+        }
+
+        private static readonly ImmutableDictionary<Section, ImmutableDictionary<string, object>> Defaults =
+            new Dictionary<Section, ImmutableDictionary<string, object>> {
+                [Section.Graphics] = new Dictionary<string, object> {
+                    ["window_maximized"] = false,
+                }.ToImmutableDictionary(),
+            }.ToImmutableDictionary();
+
+        private enum Section {
+            Graphics,
+        }
+
+        private static string AsString(this Section value) {
+            switch (value) {
+                case Section.Graphics: return "graphics";
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(value), value, null);
+            }
+        }
+    }
+}
